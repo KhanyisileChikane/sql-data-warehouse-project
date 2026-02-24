b2cc133b67b96b1bba79721abff48bbd1c02d3fe
Khanyisile Chikane <u21556769@tuks.co.za>
2026-02-24 12:19:39 +0200

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -1,1 +1,18 @@
+/*
+========================================================================
+Quality Checks
+========================================================================
+Script Purpose:
+      This script performs various quality checks for data consistency,
+      and standardization across the 'silver' schemas. It includes checks for:
+      - Null or duplicate primary keys.
+      -Unwanted spaces in string fields.
+      - Data standardization and consistency.
+      - Invalid date ranges and orders.
+      - Data consistency between related fields.
 
+Usage Notes:
+      - Run these checks after data loading silver Layer.
+      - Investigating and resolve any discrepencies found during checks.
+========================================================================
+*/
